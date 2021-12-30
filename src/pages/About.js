71009015aa52cd1ad1b61fd938a18b4a9f98e185
fearm3 <ibraaa1628@gmail.com>
2021-12-30 14:57:06 +0300

    add about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda deserunt aliquam magni provident exercitationem corrupti error, temporibus facilis. Minima quia repudiandae id nam eveniet, temporibus quis commodi voluptates quos quaerat nesciunt beatae. Quisquam amet vitae harum quo officia dicta nemo sit, ratione accusamus cum expedita ex rerum illum quia obcaecati?</p>
+    </section>
+  );
+};
 
-export default About
+export default About;
